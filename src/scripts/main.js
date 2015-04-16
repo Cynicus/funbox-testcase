98a0b5a81a67f77a60cc9ed46b9da1ed3b6c3128
Konstantin Zakablukovsky <2cynicus@gmail.com>
2015-04-16 23:20:41 +1000

    Removed bootstrap, grid changed to simple custom, fixed some errors

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 var main = function(){
 	// Use additional classes instead one with toggle for "delayed" hover effect
-	$('.item-border, .buynow').click(function(e){
+	$('.item-border path, .buynow').click(function(e){
 		var item = $(this).closest('.item');
 		if (!item.hasClass('disabled')){
 			if(item.hasClass('selected')){
